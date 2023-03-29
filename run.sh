59436cba10b4d6ed1b685ca8222eab537143d7ad
Moises Lopez <mdlopezme@gmail.com>
2023-03-28 18:40:13 -0700

    fixed a space issue

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ docker run \
     -v /dev/bus/usb:/dev/bus/usb \
     --device-cgroup-rule='c 189:* rmw' \
     --device /dev/video0 \
-    --volume='/home/jetson/.Xauthority:/ root/.Xauthority:rw' \
+    --volume='/home/jetson/.Xauthority:/root/.Xauthority:rw' \
     --volume='/tmp/.X11-unix/:/tmp/.X11-unix' \
     --volume='/home/jetson/projects/mycar:/projects/mycar' \
     docker.io/library/buildcv2
