35a5219317ca7f8b3c2a7ee3bdefcb079f9caca2
Moises Lopez <mdlopezme@gmail.com>
2023-03-30 14:18:44 -0700

    fixed typo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build --tag jetpack  --file Dockerfile.jetpack --platform linux/arm64/v8 .
 docker build --tag buildcv2 --file Dockerfile.cv2 --progress plain --platform linux/arm64/v8 .
-docker build --tag final --file Dockerfile --platfork linux/arm64/v8 .
+docker build --tag final --file Dockerfile --platform linux/arm64/v8 .
