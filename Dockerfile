4ba4ee72863469b238a91aab9baa9cf22994a1a5
Moises Lopez <mdlopezme@gmail.com>
2023-04-13 00:09:57 -0700

    name updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/buildcv2 AS cv2
+FROM ghcr.io/ucsd-ecemae-148/donkeycontainer:cv2 AS cv2
 
 ############## INSTALL ALL THE DIFFERENT FRAMEWORKs USED IN THE COURSES ##############
 RUN apt-get -y update && apt-get install -y --no-install-recommends \
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build --tag jetpack  --file Dockerfile.jetpack --platform linux/arm64/v8 .
-docker build --tag buildcv2 --file Dockerfile.cv2 --progress plain --platform linux/arm64/v8 .
-docker build --tag final --file Dockerfile --platform linux/arm64/v8 .
+docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:jetpack  --file Dockerfile.jetpack --platform linux/arm64/v8 .
+docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:cv2 --file Dockerfile.cv2 --progress plain --platform linux/arm64/v8 .
+docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:agx --file Dockerfile --platform linux/arm64/v8 .
