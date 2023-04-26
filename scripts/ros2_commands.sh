615a7ac6e69e75929580835331a89a72219ef26c
Moises Lopez <mdlopezme@gmail.com>
2023-04-26 05:03:48 -0700

    easter egg

--- scripts/ros2_commands.sh
+++ scripts/ros2_commands.sh
@@ -75,7 +75,7 @@ COLOR_RESET="\033[0m"
 
 
 if [ "$color_prompt" = yes ]; then
-    PS1='\[\033[01;34m\]Docker_Container\[\033[01;31m\]@\[\033[01;34m\]\h\[\033[01;34m\]:\[\033[01;32m\]\w\[\033[00m\]$(markup_git_branch $(git_branch))🐳 '
+    PS1='\[\033[01;34m\]Docker_Container\[\033[01;31m\]@\[\033[01;34m\]\h\[\033[01;34m\]:\[\033[01;32m\]\w\[\033[00m\]$(markup_git_branch $(git_branch))🦝 '
     # PS1='\[\033[01;34m\]Docker_Container@ucsd_robocar\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(markup_git_branch $(git_branch))🐳 '
     #PS1='\[\033[01;31m\]Docker_Container@ucsd_robocar\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] 🐳 '
     #PS1='🐳${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
