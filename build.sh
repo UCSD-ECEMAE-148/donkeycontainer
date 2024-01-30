e3947eccdb5dc1edf3d6326590782b804330d926
Moises Lopez <mdlopezme@gmail.com>
2024-01-29 17:49:11 -0800

    add base ros2 container

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:jetpack512  --file Dockerfile.jetpack --platform linux/arm64/v8 .
 # docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:cv2-490jp5 --file Dockerfile.cv2 --platform linux/arm64/v8 .
 # docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:utils --file Dockerfile.utils --platform linux/arm64/v8 .
-docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:devel --file Dockerfile --platform linux/arm64/v8 .
+docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:ros2 --file Dockerfile.ros --platform linux/arm64/v8 .
+#docker build --tag ghcr.io/ucsd-ecemae-148/donkeycontainer:devel --file Dockerfile --platform linux/arm64/v8 .
