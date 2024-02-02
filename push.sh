3187bcb1be39a272684d436dc63855a1f61e1a2a
Moises Lopez <mdlopezme@gmail.com>
2024-02-02 12:06:28 -0800

    Add tmux

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 # docker push ghcr.io/ucsd-ecemae-148/donkeycontainer:ros
 # docker push ghcr.io/ucsd-ecemae-148/donkeycontainer:utils
 # docker push ghcr.io/ucsd-ecemae-148/donkeycontainer:cv2
-docker push ghcr.io/ucsd-ecemae-148/donkeycontainer:devel
+docker push ghcr.io/ucsd-ecemae-148/donkeycontainer:ros
